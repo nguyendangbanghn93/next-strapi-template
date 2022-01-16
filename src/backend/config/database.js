4d374c154768435f198a381c21bba7b567124721
nguyendangbanghn93 <nguyendangbanghn@gmail.com>
2022-01-16 10:18:05 +0700

    config project

--- src/backend/config/database.js
+++ src/backend/config/database.js
@@ -4,17 +4,35 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        client: 'mongo',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        // client: 'mongo',
+        // host: env('DATABASE_HOST', '127.0.0.1'),
+        // port: env.int('DATABASE_PORT', 27017),
+        // database: env('DATABASE_NAME', 'strapi'),
+        // srv: env.bool('DATABASE_SRV', true),
+        "uri": "mongodb://128.199.138.254:30001/ecommerce?replicaSet=rs0"
+        // username: env('DATABASE_USERNAME', 'strapi'),
+        // password: env('DATABASE_PASSWORD', 'strapi'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE'),
         ssl: env('DATABASE_SSL'),
-      },
+      }, 
     },
   },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
